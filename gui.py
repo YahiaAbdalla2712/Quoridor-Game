abf9f44e5eaa8175209acb709506f7f13d1ec030
YahiaAbdalla2712 <yayaabdalla2712@gmail.com>
2025-12-11 14:15:25 +0200

    Add labels and 2 buttons

--- gui.py
+++ gui.py
@@ -28,6 +28,27 @@ class QuoridorGUI:
         self.player1_icon = pygame.transform.scale(self.player1_icon, (80, 80))
         self.player2_icon = pygame.transform.scale(self.player2_icon, (80, 80))
 
+        self.font = pygame.font.SysFont("arial", 28, bold=True)
+        self.text_color = (255, 255, 255)
+
+        self.ui_font = pygame.font.SysFont("arial", 30, bold=True)
+
+        self.button_reset = Button(
+            x=60, y=720, w=180, h=50,
+            text="Reset Game",
+            font=self.ui_font,
+            bg_color=(30, 140, 230),
+            text_color=(255, 255, 255)
+        )
+
+        self.button_quit = Button(
+            x=900, y=720, w=180, h=50,
+            text="Quit",
+            font=self.ui_font,
+            bg_color=(200, 50, 50),
+            text_color=(255, 255, 255)
+        )
+
     def draw_players_icons(self):
         x1 = 80
         y1 = 20
@@ -35,12 +56,36 @@ class QuoridorGUI:
         y2 = 20
         self.screen.blit(self.player1_icon, (x1, y1))
         self.screen.blit(self.player2_icon, (x2, y2))
+        label1 = self.font.render("Player 1", True, self.text_color)
+        label2 = self.font.render("Player 2", True, self.text_color)
 
+        text_x1 = x1 + (self.player1_icon.get_width() - label1.get_width()) // 2
+        text_y1 = y1 + self.player1_icon.get_height() + 5
+
+        text_x2 = x2 + (self.player2_icon.get_width() - label2.get_width()) // 2
+        text_y2 = y2 + self.player2_icon.get_height() + 5
+
+        self.screen.blit(label1, (text_x1, text_y1))
+        self.screen.blit(label2, (text_x2, text_y2))
+
+        self.button_reset.draw(self.screen)
+        self.button_quit.draw(self.screen)
+        
     def draw_pawn(self, image, x, y):
         px = (x + 5) * self.cell_size + (self.cell_size - self.pawn_size) // 2
         py = (y + 2) * self.cell_size + (self.cell_size - self.pawn_size) // 2
         self.screen.blit(image, (px, py))
 
+    def write_turn(self):
+        label1 = self.font.render("Turn", True, self.text_color)
+        self.screen.blit(label1, ((self.screen.get_width()/2)-30, 30))
+
+    def write_walls_left(self):
+        label1 = self.font.render("Walls left:", True, self.text_color)
+        label2 = self.font.render("Walls left:", True, self.text_color)
+        self.screen.blit(label1, (60, 200))
+        self.screen.blit(label2, (self.screen.get_width()-190, 200))
+        
     def draw_board(self):
         self.screen.fill((80, 50, 150))
         # Draw grid
@@ -55,7 +100,9 @@ class QuoridorGUI:
         self.draw_pawn(self.pawn_p1_img, p1_x, p1_y)
         self.draw_pawn(self.pawn_p2_img, p2_x, p2_y)
         self.draw_players_icons()
-
+        self.write_turn()
+        self.write_walls_left()
+        
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -72,4 +119,5 @@ if __name__ == "__main__":
     players = [Player("Player1"), Player("Player2", is_ai=True)]
     gui = QuoridorGUI(board, players)
     gui.game_loop()
+
 
